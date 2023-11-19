5e1c9dad63e4fc79af5c2c23f474d8f0c7cf0520
Rumeysapat <rumeysapatt@gmail.com>
2023-11-19 16:22:02 +0300

    Mvc template yapısının uygulanması

--- Store/StoreApp/Program.cs
+++ Store/StoreApp/Program.cs
@@ -1,6 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+
+app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+
+app.UseRouting();
+app.UseHttpsRedirection();
+
 
 app.Run();
