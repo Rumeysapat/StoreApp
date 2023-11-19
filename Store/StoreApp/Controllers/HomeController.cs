57a3163e3515bdd299b9dd088946d586388b5292
Rumeysapat <rumeysapatt@gmail.com>
2023-11-19 16:40:53 +0300

    home controller düzenlendi.

---
+++ Store/StoreApp/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace StoreApp.Controllers
+{
+
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+
+
+    }
+}
+
+
