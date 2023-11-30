e2c20256fa17995b176abad91e1a5ac97516dcd5
Rumeysapat <rumeysapatt@gmail.com>
2023-11-30 21:33:20 +0300

    ürünün id sine göre ekrana daha fazla bilgi getirir.

--- Store/StoreApp/Controllers/ProductController.cs
+++ Store/StoreApp/Controllers/ProductController.cs
@@ -1,5 +1,6 @@
 using System.Reflection.Metadata.Ecma335;
 using Microsoft.AspNetCore.Mvc;
+using StoreApp.Models;
 
 
 
@@ -26,6 +27,13 @@ namespace StoreApp.Controllers
             var model = _context.Products.ToList();
             return View (model);
         }
+        public IActionResult Get (int id)
+        {
+           Product product = _context.Products.First(p=>p.ProductId.Equals(id));
+
+             return View (product);
+        }
+
 
 
     }
