7049d5cccbccb36b9008eecfec13c64366e31e04
Rumeysapat <rumeysapatt@gmail.com>
2023-12-16 16:51:06 +0300

    LibMan ile client-side tarafında çalışacak kütüphaneleri ekledik

--- Store/StoreApp/Program.cs
+++ Store/StoreApp/Program.cs
@@ -2,10 +2,12 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<RepositoryContext>(options=>options.UseSqlite(builder.Configuration.GetConnectionString("SqlConnection")));
+builder.Services.AddDbContext<RepositoryContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("SqlConnection")));
 
 
 var app = builder.Build();
+app.UseStaticFiles();
+
 
 
 
@@ -13,6 +15,7 @@ app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
 
 app.UseRouting();
 app.UseHttpsRedirection();
+
 
 
 app.Run();
